a6671ae87b12d9d6ec80cab0fa209614d856752d
TwilightValley2017 <tenderyoung@yeah.net>
2022-12-09 00:26:47 +0800

    docs: 踩坑文档

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,14 +3,14 @@ module.exports = {
         "browser": true,
         "commonjs": true,
         "es2021": true,
-        "node": true, // https://www.codegrepper.com/tpc/%27__dirname%27+is+not+defined.eslintno-undef
+        "node": true,
     },
     "extends": "eslint:recommended",
     "overrides": [
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
-        "sourceType": "module", // https://www.jianshu.com/p/2d6550a83f06 | https://juejin.cn/post/7025870140537307150
+        "sourceType": "module",
     },
     "rules": {
       semi: ["error", "never"]
---
+++ README.md
@@ -0,0 +1,11 @@
+# untitled
+## eslint
+
+### .eslintignore
+- https://zhuanlan.zhihu.com/p/37918096
+### evn.node
+- https://www.codegrepper.com/tpc/%27__dirname%27+is+not+defined.eslintno-undef
+
+### parserOptions.sourceType
+- https://www.jianshu.com/p/2d6550a83f06
+- https://juejin.cn/post/7025870140537307150

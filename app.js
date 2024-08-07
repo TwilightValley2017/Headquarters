c6b9bdb68173974e2ac7565e4edb37d379d26a46
yangyang1 <yangyang1@lenovocloud.com>
2024-08-07 18:39:23 +0800

    feat: 添加吸顶和堆叠效果

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const { PORT } = require("./port.module")
 
 const headquartersApp = express()
 
-headquartersApp.use(express.static(path.join(__dirname, "public")))
+headquartersApp.use(express.static(path.join(__dirname, "shooting-range")))
 headquartersApp.use(express.static(path.join(__dirname, "dist")))
 
 headquartersApp.get("/hello", (req, res) => {

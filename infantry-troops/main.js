6e099900688292b7b46c370dd3897a402dd9f361
TwilightValley2017 <tenderyoung@yeah.net>
2024-08-09 11:43:37 +0800

    feat: 更新 pnpm

--- infantry-troops/main.js
+++ infantry-troops/main.js
@@ -1,6 +1,8 @@
 import ITEMS from './supplement'
 
 window.addEventListener("load", () => {
+    console.log("infantry troops ready!")
+
     const path = ITEMS.TRY_CATCH
 
     import(/* webpackChunkName: "chunk" */ `./src/${path}`)

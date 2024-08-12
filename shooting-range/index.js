84c21ffd557c4739f03b62998a3e495cfab0889f
yangyang1 <yangyang1@lenovocloud.com>
2024-08-12 11:19:30 +0800

    feat: 添加滚动交叉识别触发缩放

--- shooting-range/index.js
+++ shooting-range/index.js
@@ -2,7 +2,9 @@ document.addEventListener("DOMContentLoaded", () => {
   console.log("DOM 完全加载和解析")
 
   // 开始监听
-  certCard1.observe(document.querySelector(".cert-card:nth-child(1)"))
+  // certCard1.observe(document.querySelector(".cert-card:nth-child(1)"))
+  certCard2.observe(document.querySelector(".cert-card:nth-child(2)"))
+  certCard3.observe(document.querySelector(".cert-card:nth-child(3)"))
 })
 
 const options = {
@@ -11,15 +13,71 @@ const options = {
   threshold: [0, 0.25, 0.5, 0.75, 1],
 }
 
-const certCard1 = new IntersectionObserver((entries) => {
-  // 如果 intersectionRatio 为 0，则目标在视野外，
-  // 我们不需要做任何事情。
-  if (entries[0].intersectionRatio <= 0) return
+// const certCard1 = new IntersectionObserver((entries) => {
+//   // 如果 intersectionRatio 为 0，则目标在视野外，
+//   // 我们不需要做任何事情。
+//   if (entries[0].intersectionRatio <= 0) {
+//     return
+//   }
 
-  doTransform(10)
-  console.log("Loaded new items")
+//   certCardTransformHandler1(entries[0])
+// }, options)
+
+const certCard2 = new IntersectionObserver((entries) => {
+  // 如果 intersectionRatio 为 0，则目标在 viewport 外，我们不需要做任何事情。
+  if (entries[0].intersectionRatio <= 0) {
+    return
+  }
+
+  certCardIntersectionHandler2(entries[0])
 }, options)
 
-const doTransform = (e) => {
-  console.log(e)
+const certCard3 = new IntersectionObserver((entries) => {
+  // 如果 intersectionRatio 为 0，则目标在 viewport 外，我们不需要做任何事情。
+  if (entries[0].intersectionRatio <= 0) {
+    return
+  }
+
+  certCardIntersectionHandler3(entries[0])
+}, options)
+
+// const certCardTransformHandler1 = (e) => {
+//   if (e.intersectionRatio > 0.99) {
+//     e.target.classList.add('####')
+//     console.warn({ e, inter: e.intersectionRect, ratio: e.intersectionRatio })
+//   }
+// }
+
+const certCardIntersectionHandler2 = (e) => {
+  const div = document.querySelector(".cert-card:nth-child(1)")
+  const h1 = document.querySelector(".cert-card:nth-child(1) h1")
+
+  if (e.intersectionRatio >= 0.2 && e.intersectionRatio <= 0.3) {
+    div.classList.add('scale-90')
+  }
+
+  if (e.intersectionRatio >= 0.7 && e.intersectionRatio <= 0.8) {
+    div.classList.add('scale-80')
+  }
+
+  if (e.intersectionRatio >= 0.95) {
+    h1.classList.add('scale-80')
+  }
+
+  console.warn({ ele: div, e, inter: e.intersectionRect, ratio: e.intersectionRatio })
+}
+
+const certCardIntersectionHandler3 = (e) => {
+  const div = document.querySelector(".cert-card:nth-child(2)")
+  const h1 = document.querySelector(".cert-card:nth-child(2) h1")
+
+  if (e.intersectionRatio >= 0.2 && e.intersectionRatio <= 0.3) {
+    div.classList.add('scale-90')
+  }
+
+  if (e.intersectionRatio >= 0.95) {
+    h1.classList.add('scale-90')
+  }
+
+  console.warn({ ele: div, e, inter: e.intersectionRect, ratio: e.intersectionRatio })
 }

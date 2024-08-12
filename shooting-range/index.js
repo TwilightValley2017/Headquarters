4e1119062988c2c368fc608ad72df6aeafc38288
yangyang1 <yangyang1@lenovocloud.com>
2024-08-13 01:23:21 +0800

    feat: sticky 效果调优

--- shooting-range/index.js
+++ shooting-range/index.js
@@ -1,41 +1,42 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM 完全加载和解析")
+  console.log("DOM Loaded")
 
   // 开始监听
-  // certCard1.observe(document.querySelector(".cert-card:nth-child(1)"))
-  certCard2.observe(document.querySelector(".cert-card:nth-child(2)"))
-  certCard3.observe(document.querySelector(".cert-card:nth-child(3)"))
+  card2.observe(document.querySelector(".card:nth-child(2)"))
+  card3.observe(document.querySelector(".card:nth-child(3)"))
 })
 
+const threshold = [0, 0.25, 0.35, 0.65, 0.75, 0.85, 0.95, 1]
+// const threshold = [0, 0.5, 1]
 const options = {
-  root: document.querySelector(".cert-container"),
+  root: document.querySelector(".card-container"),
   rootMargin: "0px",
-  threshold: [0, 0.25, 0.35, 0.65, 0.75, 0.85, 0.95, 1],
+  threshold: threshold,
 }
 
-const certCard2 = new IntersectionObserver((entries) => {
+const card2 = new IntersectionObserver((entries) => {
   // 如果 intersectionRatio 为 0，则目标在 viewport 外，我们不需要做任何事情。
   if (entries[0].intersectionRatio < 0) {
     return
   }
 
-  certCardIntersectionHandler2(entries[0])
+  cardIntersectionHandler2(entries[0])
 }, options)
 
-const certCard3 = new IntersectionObserver((entries) => {
+const card3 = new IntersectionObserver((entries) => {
   // 如果 intersectionRatio 为 0，则目标在 viewport 外，我们不需要做任何事情。
   if (entries[0].intersectionRatio <= 0) {
     return
   }
 
-  certCardIntersectionHandler3(entries[0])
+  cardIntersectionHandler3(entries[0])
 }, options)
 
-const certCardIntersectionHandler2 = (e) => {
-  const div = document.querySelector(".cert-card:nth-child(1)")
-  ,h1 = document.querySelector(".cert-card:nth-child(1) h1")
+const cardIntersectionHandler2 = (e) => {
+  const div = document.querySelector(".card:nth-child(1)")
+  ,h1 = document.querySelector(".card:nth-child(1) h1")
   ,scale90 = ['scale-90', 'scale-center-top', 'scale-transition']
-  ,scale80 = ['scale-80', 'scale-center-top', 'scale-transition', 'opacity-lowest']
+  ,scale80 = ['scale-80', 'scale-center-top', 'scale-transition']
   ,scale80h1 = ['scale-80', 'scale-center', 'scale-transition']
 
   // out-in
@@ -81,21 +82,17 @@ const certCardIntersectionHandler2 = (e) => {
   console.warn({ e, inter: e.intersectionRect, ratio: e.intersectionRatio })
 }
 
-const certCardIntersectionHandler3 = (e) => {
-  const div = document.querySelector(".cert-card:nth-child(2)")
-  ,h1 = document.querySelector(".cert-card:nth-child(2) h1")
-  ,scale90 = ['scale-90', 'scale-center-top', 'scale-transition', 'opacity-lower']
+const cardIntersectionHandler3 = (e) => {
+  const div = document.querySelector(".card:nth-child(2)")
+  ,h1 = document.querySelector(".card:nth-child(2) h1")
+  ,scale90 = ['scale-90', 'scale-center-top', 'scale-transition']
   // ,scale80 = ['scale-80', 'scale-center-top', 'scale-transition']
-  ,scale90h1 = ['scale-85', 'scale-center', 'scale-transition']
+  ,scale90h1 = ['scale-90', 'scale-center', 'scale-transition']
 
   // out-in
   if (e.intersectionRatio >= 0.3 && e.intersectionRatio < 0.4) {
     div.classList.add(...scale90)
   }
-
-  // if (e.intersectionRatio >= 0.7 && e.intersectionRatio < 0.8) {
-  //   div.classList.add('scale-80', 'scale-transition')
-  // }
 
   if (e.intersectionRatio > 0.9 && e.intersectionRatio < 1) {
     h1.classList.add(...scale90h1)
@@ -108,12 +105,6 @@ const certCardIntersectionHandler3 = (e) => {
     }
   }
 
-  // if (e.intersectionRatio >= 0.6 && e.intersectionRatio < 0.7) {
-  //   if (div.classList.contains('scale-80')) {
-  //     div.classList.remove('scale-80', 'scale-transition')
-  //   }
-  // }
-
   if (e.intersectionRatio >= 0.8 && e.intersectionRatio < 0.9) {
     if (h1.classList.contains('scale-90')) {
       h1.classList.remove(...scale90h1)
@@ -122,10 +113,8 @@ const certCardIntersectionHandler3 = (e) => {
 
   if (e.intersectionRatio <= 0.1) {
     if (e.boundingClientRect.bottom > 0) {
-      h1.classList.remove(...scale90h1)
       div.classList.remove(...scale90)
+      h1.classList.remove(...scale90h1)
     }
   }
-
-  console.warn({ e, inter: e.intersectionRect, ratio: e.intersectionRatio })
 }

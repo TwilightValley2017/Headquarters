fd7043dbce99eba7ea64b06cae9e3802a05e06c8
yangyang1 <yangyang1@lenovocloud.com>
2024-08-12 13:04:37 +0800

    feat: 添加滚动交叉识别缩放还原

--- shooting-range/index.js
+++ shooting-range/index.js
@@ -10,7 +10,7 @@ document.addEventListener("DOMContentLoaded", () => {
 const options = {
   root: document.querySelector(".cert-container"),
   rootMargin: "0px",
-  threshold: [0, 0.25, 0.5, 0.75, 1],
+  threshold: [0, 0.25, 0.35, 0.65, 0.75, 0.85, 0.95, 1],
 }
 
 // const certCard1 = new IntersectionObserver((entries) => {
@@ -25,7 +25,7 @@ const options = {
 
 const certCard2 = new IntersectionObserver((entries) => {
   // 如果 intersectionRatio 为 0，则目标在 viewport 外，我们不需要做任何事情。
-  if (entries[0].intersectionRatio <= 0) {
+  if (entries[0].intersectionRatio < 0) {
     return
   }
 
@@ -50,34 +50,99 @@ const certCard3 = new IntersectionObserver((entries) => {
 
 const certCardIntersectionHandler2 = (e) => {
   const div = document.querySelector(".cert-card:nth-child(1)")
-  const h1 = document.querySelector(".cert-card:nth-child(1) h1")
+  ,h1 = document.querySelector(".cert-card:nth-child(1) h1")
+  ,scale90 = ['scale-90', 'scale-center-top', 'scale-transition']
+  ,scale80 = ['scale-80', 'scale-center-top', 'scale-transition', 'opacity-lowest']
+  ,scale80h1 = ['scale-80', 'scale-center', 'scale-transition']
 
-  if (e.intersectionRatio >= 0.2 && e.intersectionRatio <= 0.3) {
-    div.classList.add('scale-90')
+  // out-in
+  if (e.intersectionRatio >= 0.3 && e.intersectionRatio < 0.4) {
+    div.classList.add(...scale90)
   }
 
-  if (e.intersectionRatio >= 0.7 && e.intersectionRatio <= 0.8) {
-    div.classList.add('scale-80')
+  if (e.intersectionRatio >= 0.7 && e.intersectionRatio < 0.8) {
+    div.classList.add(...scale80)
   }
 
-  if (e.intersectionRatio >= 0.95) {
-    h1.classList.add('scale-80')
+  if (e.intersectionRatio > 0.9 && e.intersectionRatio < 1) {
+    h1.classList.add(...scale80h1)
   }
 
-  console.warn({ ele: div, e, inter: e.intersectionRect, ratio: e.intersectionRatio })
+  // in-out
+  if (e.intersectionRatio >= 0.2 && e.intersectionRatio < 0.3) {
+    if (div.classList.contains('scale-90')) {
+      div.classList.remove(...scale90)
+    }
+  }
+
+  if (e.intersectionRatio >= 0.6 && e.intersectionRatio < 0.7) {
+    if (div.classList.contains('scale-80')) {
+      div.classList.remove(...scale80)
+    }
+  }
+
+  if (e.intersectionRatio >= 0.8 && e.intersectionRatio < 0.9) {
+    if (h1.classList.contains('scale-80')) {
+      h1.classList.remove(...scale80h1)
+    }
+  }
+
+  if (e.intersectionRatio <= 0.1) {
+    if (e.boundingClientRect.bottom > 0) {
+      h1.classList.remove(...scale80h1)
+      div.classList.remove(...scale80)
+      div.classList.remove(...scale90)
+    }
+  }
+
+  console.warn({ e, inter: e.intersectionRect, ratio: e.intersectionRatio })
 }
 
 const certCardIntersectionHandler3 = (e) => {
   const div = document.querySelector(".cert-card:nth-child(2)")
-  const h1 = document.querySelector(".cert-card:nth-child(2) h1")
+  ,h1 = document.querySelector(".cert-card:nth-child(2) h1")
+  ,scale90 = ['scale-90', 'scale-center-top', 'scale-transition', 'opacity-lower']
+  // ,scale80 = ['scale-80', 'scale-center-top', 'scale-transition']
+  ,scale90h1 = ['scale-90', 'scale-center', 'scale-transition']
 
-  if (e.intersectionRatio >= 0.2 && e.intersectionRatio <= 0.3) {
-    div.classList.add('scale-90')
+  // out-in
+  if (e.intersectionRatio >= 0.3 && e.intersectionRatio < 0.4) {
+    div.classList.add(...scale90)
   }
 
-  if (e.intersectionRatio >= 0.95) {
-    h1.classList.add('scale-90')
+  // if (e.intersectionRatio >= 0.7 && e.intersectionRatio < 0.8) {
+  //   div.classList.add('scale-80', 'scale-transition')
+  // }
+
+  if (e.intersectionRatio > 0.9 && e.intersectionRatio < 1) {
+    h1.classList.add(...scale90h1)
   }
 
-  console.warn({ ele: div, e, inter: e.intersectionRect, ratio: e.intersectionRatio })
+  // in-out
+  if (e.intersectionRatio >= 0.2 && e.intersectionRatio < 0.3) {
+    if (div.classList.contains('scale-90')) {
+      div.classList.remove(...scale90)
+    }
+  }
+
+  // if (e.intersectionRatio >= 0.6 && e.intersectionRatio < 0.7) {
+  //   if (div.classList.contains('scale-80')) {
+  //     div.classList.remove('scale-80', 'scale-transition')
+  //   }
+  // }
+
+  if (e.intersectionRatio >= 0.8 && e.intersectionRatio < 0.9) {
+    if (h1.classList.contains('scale-90')) {
+      h1.classList.remove(...scale90h1)
+    }
+  }
+
+  if (e.intersectionRatio <= 0.1) {
+    if (e.boundingClientRect.bottom > 0) {
+      h1.classList.remove(...scale90h1)
+      div.classList.remove(...scale90)
+    }
+  }
+
+  console.warn({ e, inter: e.intersectionRect, ratio: e.intersectionRatio })
 }
